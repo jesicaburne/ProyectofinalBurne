2ce0ed473f982be69e013ee1ca5e772339134a67
jesica burne <jesicaburne@hotmail.com>
2022-08-12 10:15:56 -0300

    componente redes

--- proyectofinal/src/app/app.module.ts
+++ proyectofinal/src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {formsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componets/header/header.component';
+import { RedesComponent } from './componets/redes/redes.component';
+import { LogoapComponent } from './componets/logoap/logoap.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    RedesComponent,
+    LogoapComponent,
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    formsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
