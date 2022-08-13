d2f7ed4af3648ffa876791d3347da656609429ca
jesica burne <jesicaburne@hotmail.com>
2022-08-13 07:37:28 -0300

    hys

--- proyectofinal/src/app/app.module.ts
+++ proyectofinal/src/app/app.module.ts
@@ -3,12 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannersComponent } from './components/banners/banners.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { RedesComponent } from './components/redes/redes.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannersComponent,
+    HeaderComponent,
+    LogoApComponent,
+    ExperienciaComponent,
+    RedesComponent,
+    AcercaDeComponent,
+    EducacionComponent
   ],
   imports: [
+    NgCircleProgressModule.forRoot({}),
     BrowserModule,
     AppRoutingModule
   ],
