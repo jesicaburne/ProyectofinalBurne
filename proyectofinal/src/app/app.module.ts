6c92d79c3e9c3337cf228eb1979f1debfebf2bb4
jesica burne <jesicaburne@hotmail.com>
2022-08-13 14:10:11 -0300

    componente footer

--- proyectofinal/src/app/app.module.ts
+++ proyectofinal/src/app/app.module.ts
@@ -8,12 +8,13 @@ import { BannersComponent } from './components/banners/banners.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoApComponent } from './components/logo-ap/logo-ap.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
-import { RedesComponent } from './components/redes/redes.component';
+
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -22,11 +23,11 @@ import { ProyectoComponent } from './components/proyecto/proyecto.component';
     HeaderComponent,
     LogoApComponent,
     ExperienciaComponent,
-    RedesComponent,
     AcercaDeComponent,
     EducacionComponent,
     HysComponent,
-    ProyectoComponent
+    ProyectoComponent,
+    FooterComponent
   ],
   imports: [
     NgCircleProgressModule.forRoot({}),
