4d3f38da0c2a71bea70a1fec67cb3853546ae6f5
jesica burne <jesicaburne@hotmail.com>
2022-10-30 16:16:14 -0300

    service

--- src/app/service/token.service.ts
+++ src/app/service/token.service.ts
@@ -18,6 +18,32 @@ roles: Array<string> =[];
     window.sessionStorage.setItem(TOKEN_KEY,token);
   }
   public getToken():string{
-    return sessionStorage.getItem(TOKEN_KEY!);
+    return sessionStorage.getItem(TOKEN_KEY)!;
   }
+
+  public setUserName(userName: string): void{
+    window.sessionStorage.removeItem(USERNAME_KEY);
+    window.sessionStorage.setItem(USERNAME_KEY, userName);
+  }
+
+  public getUserName():string{
+    return sessionStorage.getItem(USERNAME_KEY)!;
+  }
+  public setAuthorities(authorities: string[]):void{
+    window.sessionStorage.removeItem(AUTHORITIES_KEY);
+    window.sessionStorage.setItem(AUTHORITIES_KEY, JSON.stringify(authorities));
+
+  }
+  public getAuthorities(): string[]{
+    this.roles = [];
+    if (sessionStorage.getItem(AUTHORITIES_KEY)){
+      JSON.parse(sessionStorage.getItem(AUTHORITIES_KEY)!).forEach(authority:any =>{ 
+        this.roles.push(authority.authority);
+      });
+    }  
+    return this.roles;
+    
+}
+public logOut(): void{
+  window.sessionStorage.clear();
 }
