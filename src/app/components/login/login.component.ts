2d389dbf16409e080660fb42acb567296e9e71fc
jesica burne <jesicaburne@hotmail.com>
2022-10-30 18:28:17 -0300

    finalice conex login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  isLogged = false;
+  isLogginDail= false;
+  loginUsuario! : LoginUsuario;
+  nombreUsuario! : string;
+  password! : string;
+  roles: string[] = [];
+  errMsj!: string;
+
+  constructor(private tokenService: TokenService, private AuthService:AuthService, private router: router) { }
 
   ngOnInit(): void {
+    if (this.tokenService.getToken()){
+      this.isLogged = true;
+      this.isLogginFail = false;
+      this.roles =this.tokenService.getAuthorities();
+    }
+  }
+  onLogin():void{
+    this.loginUsuario = new LoginUsuario(this.nombreUsuario, this.password);
+     this.AuthService.login(this.loginUsuario).subscribe(data=> {
+        this.isLogged=true; 
+        this.isLogginFail=false;
+        this.tokenService.setToken(data.token));
+        this.tokenService.setUserName(data.nombreUsuario);
+        this.tokenService.setAuthorities(data.authorities);
+        this.roles = data.authorities;
+        this.router.navigate([' '])
+      },err =>{
+        this.isLogged =false;
+        this.isLogginFail= true;
+        this.errMsj =err.error.mensaje;
+        console.log(this.errMsj);
+      }
+    
+
   }
 
-}
