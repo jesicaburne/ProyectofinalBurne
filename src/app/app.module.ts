5de3fc280a73d3d3233c1db53e7b2ebf5742ddac
jesica burne <jesicaburne@hotmail.com>
2022-10-30 13:49:51 -0300

    conexion

---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BannersComponent } from './components/banners/banners.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './components/hys/hys.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
+import {HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BannersComponent,
+    HeaderComponent,
+    LogoApComponent,
+    ExperienciaComponent,
+    AcercaDeComponent,
+    EducacionComponent,
+    HysComponent,
+    ProyectoComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
+  ],
+  imports: [
+    NgCircleProgressModule.forRoot({}),
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
